382f1435a074473ca422d4fecc80d29077e668c6
YU3727 <lacvert3727@naver.com>
2023-02-18 15:07:49 +0900

    230218

--- src/main/java/com/rabbit/s1/notice/NoticeDAO.java
+++ src/main/java/com/rabbit/s1/notice/NoticeDAO.java
@@ -1,5 +1,7 @@
 package com.rabbit.s1.notice;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -10,6 +12,19 @@ public class NoticeDAO {
 	@Autowired
 	private SqlSession sqlSession;
 	private final String NAMESPACE = "com.rabbit.s1.notice.NoticeDAO.";
+	
+	
+	public List<NoticeDTO> getNoticeList() throws Exception{
+		return sqlSession.selectList(NAMESPACE+"getNoticeList");
+	}
+	
+	public NoticeDTO getNoticeDetail(NoticeDTO noticeDTO) throws Exception{
+		return sqlSession.selectOne(NAMESPACE+"getNoticeDetail", noticeDTO);
+	}
+	
+	public int setNoticeAdd(NoticeDTO noticeDTO) throws Exception{
+		return sqlSession.insert(NAMESPACE+"setNoticeAdd", noticeDTO);
+	}
 	
 	
 }
